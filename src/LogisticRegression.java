4e932033c4bae3aafd9235e0adcba9f7645e2e14
BenHuiHui <qinghe1992@gmail.com>
2015-10-21 11:23:31 +0800

    .

--- src/LogisticRegression.java
+++ src/LogisticRegression.java
@@ -5,8 +5,8 @@ public class LogisticRegression {
 
     private double[] weights;
     private double rate = 0.0001;
-    //private double threshold = 0.01;
-    private int numOfIterations = 5000;
+    private double threshold = 1;
+    private int numOfIterations = 10000;
 
     public LogisticRegression(int n){
         this.weights = new double[n];
@@ -28,31 +28,9 @@ public class LogisticRegression {
      */
     public double[] train (List<Instance>instances){
 
-        System.out.println("Training");
+        //System.out.println("Training");
 
         for (int i = 0; i < numOfIterations; i++) {
-
-            //double error = 0.0;
-
-            //Batch gradient descent
-            /*
-            for (int j = 0; j < weights.length; j++) {
-
-                double dir = 0.0;
-
-                for (int k = 0; k < instances.size(); k++) {
-
-                    Instance instance = instances.get(k);
-                    double predicted = classify(instance);
-                    int label = instance.label;
-                    dir += (label - predicted) * instance.features[k];
-                }
-
-                weights[j] = weights[j] + rate / instances.size() * dir;
-                error += dir * dir;
-            }
-            */
-
 
             //Stochastic gradient descent
 
@@ -79,29 +57,13 @@ public class LogisticRegression {
             }
             err = Math.sqrt(err);
 
-            if (err < 1) break;
+            if (err < threshold) break;
             //System.out.println(err);
         }
 
         return weights;
     }
 
-    /*
-    public List<Integer> classify (List<Instance> instances){
-        List<Integer> results = new ArrayList<Integer>();
-
-        for (Instance instance: instances){
-            if (classify(instance) >= 0.5){
-                results.add(1);
-            }
-            else{
-                results.add(0);
-            }
-        }
-
-        return results;
-    }
-    */
 
     public int classifySingle(Instance instance){
         //System.out.println(classify(instance));
